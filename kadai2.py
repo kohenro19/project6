ec345cb2ecc0e51a364197fdbd0189b30ed210c0
kohenro19 <kohenro19@gmail.com>
2021-10-26 21:37:40 +0900

    20211026r1

--- kadai2.py
+++ kadai2.py
@@ -1,23 +1,25 @@
 import requests
-import numpy as np
-import pandas as pd
+import json
 
 REQUEST_URL = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706"
 APP_ID=1035694033279278134
 
-serch_keyword = 'メークイン'
+serch_keyword = '米'
 
 serch_params = {
     "format" : "json",
     "keyword" : serch_keyword,
     "applicationId" : [APP_ID],
-    "availability" : 0,
+    "availability" : 1,
     "hits" : 30,
     "page" : 1,
     "sort" : "-updateTimestamp"
 }
 
 response = requests.get(REQUEST_URL, serch_params)
-result = response.json()
+response = response.json()
 
-print(result)
+for list in response["Items"]:
+    # print(list)
+    print(list["Item"]["itemPrice"])
+
