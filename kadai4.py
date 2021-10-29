41e6bccec4e4ace4b3a541805738e1bcb4e27356
kohenro19 <kohenro19@gmail.com>
2021-10-29 20:43:48 +0900

    20211029r1

---
+++ kadai4.py
@@ -0,0 +1,29 @@
+import requests
+
+# 定数は上部で定義する
+RAKUTEN_PRODUCT_URL = "https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20170628?"
+APP_ID = "1035694033279278134"
+
+serch_keyword = '米'
+
+serch_params = {
+    "format" : "json",
+    "keyword" : serch_keyword,
+    "applicationId" : [APP_ID],
+    "availability" : 1,
+    "hits" : 30,
+    "page" : 1,
+
+}
+
+response = requests.get(RAKUTEN_PRODUCT_URL, serch_params)
+response = response.json()
+
+for list in response["Items"]:
+    global tmp
+    # print(list["Item"])
+    tmp = str(list["Item"]["rank"]) + ": " + list["Item"]["itemName"] + "\r\n"
+
+    with open("output.txt", mode='a', encoding="utf8") as f:
+        f.write(tmp)
+        f.close
